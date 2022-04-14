7f724a2478633db738a2e14e8f6fc8fb6e83f113
Bala <balasubramani.kuppusamy@ymedialabs.com>
2022-04-14 15:05:09 +0530

    commit1

---
+++ scripts/docker-run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cd $( dirname "$0" )/..
+
+port=${80}
+
+# Stop the container, don't wait to kill it
+docker stop --time 0 kubeweb > /dev/null 2>&1
+
+# Remove the container
+docker rm kubeweb > /dev/null 2>&1
+
+echo "Running. To stop:\ndocker stop --time 0 octochat"
+
+# Run the container
+container=$( docker run --name kubeweb \
+  -p $port:80 \
+  --detach \
+  kubeweb )
+
+open http://localhost:$port/
+
+docker logs --follow $container
